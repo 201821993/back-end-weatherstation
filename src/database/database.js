f7d22c26eef17c1d3cfbe9510e76da8efb2611fd
201821993 <201821993@tese.edu.mx>
2024-03-11 12:40:57 -0600

    11-03-2024 first commit uploading the repository to GitHub

---
+++ src/database/database.js
@@ -0,0 +1,20 @@
+// configuración de mysql y la base de datos 
+
+import mysql from "promise-mysql";
+import config from "../config.js";
+
+const connection = mysql.createConnection({
+    host:config.host,
+    user:config.user,
+    password:config.password,
+    database: config.database,
+    port :config.port
+},{multipleStatement:true});
+
+
+const getConnection = () =>{
+    return connection;
+}
+module.exports = {
+    getConnection
+}
